1229da99cd381f5ba9585c3c95c99f633c71820b
Alexander Sosedkin <asosedkin@redhat.com>
2022-12-09 13:26:22 +0100

    0-prepare: don't install *all* diffoscope weak deps

--- 0-prepare.sh
+++ 0-prepare.sh
@@ -2,7 +2,8 @@
 set -uex
 
 rpm -q rpmbuild || sudo dnf -y install rpm-build
-rpm -q diffoscope || sudo dnf -y install diffoscope
+rpm -q diffoscope || \
+	sudo dnf -y --setopt=install_weak_deps=False install diffoscope
 rpm -q disorderfs || sudo dnf -y install disorderfs
 rpm -q reprotest || sudo dnf -y install reprotest
 sudo dnf -y builddep 1-hello-srpm/hello.spec
